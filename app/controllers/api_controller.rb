7ee00ceec2d0066bfdb091be4ddadbcd78f27901
valerii26 <svv2602@gmail.com>
2023-08-04 13:32:15 +0300

    Добавлено первичное форматирование ячеек в прайсе

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -93,15 +93,33 @@ class ApiController < ApplicationController
     xls_file = Spreadsheet::Workbook.new
     xls_sheet = xls_file.create_worksheet(name: @sheet_name)
 
+    # Создание стиля для зеленого фона
+    green_background = Spreadsheet::Format.new(color: :white, pattern: 1,
+                                               pattern_fg_color: :green,
+                                               border: :thin)
+    # Создание стиля с границей
+    border_style = Spreadsheet::Format.new(border: :thin, color: :black)
+
     # Добавление заголовков в таблицу XLS
     column_names = hash_grouped_name_collumns(hash_with_params_sklad)[:attr_query_name_collumn]
-
     xls_sheet.row(0).concat column_names
+
+    # Применение стиля к каждой ячейке заголовков, если она содержит значение
+    column_names.each_with_index do |value, col_index|
+      if value.present?
+        xls_sheet.row(0).set_format(col_index, green_background)
+      end
+    end
 
     # Заполнение таблицы данными
     grouped_results.each_with_index do |leftover, index|
       row_values = column_names.map { |column| leftover.send(column) }
       xls_sheet.row(index + 1).push(*row_values)
+
+      # Применение стиля с границей к каждой ячейке в строках с данными
+      row_values.each_with_index do |cell_value, col_index|
+        xls_sheet.row(index + 1).set_format(col_index, border_style)
+      end
     end
 
 
