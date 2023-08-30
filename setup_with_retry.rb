2022355182f8375511420b6a378468faab2fffb5
valerii26 <svv2602@gmail.com>
2023-08-30 22:56:56 +0300

    Добавлен в config/application.rb алгоритм при ошибке блокировки базы данных

---
+++ setup_with_retry.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# Удаление файлов всех баз данных (при необходимости)
+files_to_delete = Dir.glob("#{Rails.root}/db/*.sqlite3")
+
+files_to_delete.each do |file_path|
+  File.delete(file_path)
+  puts "Удален файл: #{file_path}"
+end
+
+# Запуск rails db:setup
+system('bundle exec rails db:setup')
